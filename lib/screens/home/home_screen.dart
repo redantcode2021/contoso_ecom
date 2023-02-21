62ed0e42bb04824adda246f4e973a8aaa2037230
redantcode2021 <redantcyber@gmail.com>
2023-02-21 07:47:00 +0700

    5-add-product-carousel-on-home-screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
-import '../../models/category_model.dart';
+import '../../models/models.dart';
 import '../../widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -24,17 +24,35 @@ class HomeScreen extends StatelessWidget {
         automaticallyImplyLeading: true,
       ),
       bottomNavigationBar: const CustomNavBar(),
-      body: Container(
-        child: CarouselSlider(
-          options: CarouselOptions(
-            aspectRatio: 1.5,
-            viewportFraction: 0.9,
-            enlargeCenterPage: true,
-            enlargeStrategy: CenterPageEnlargeStrategy.height,
-          ),
-          items: Category.categories
-              .map((category) => HeroCarouselCard(category: category))
-              .toList(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              child: CarouselSlider(
+                options: CarouselOptions(
+                  aspectRatio: 1.5,
+                  viewportFraction: 0.9,
+                  enlargeCenterPage: true,
+                  enlargeStrategy: CenterPageEnlargeStrategy.height,
+                ),
+                items: Category.categories
+                    .map((category) => HeroCarouselCard(category: category))
+                    .toList(),
+              ),
+            ),
+            const SectionTitle(title: 'RECOMMENDED'),
+            ProductCarousel(
+              products: Product.products
+                  .where((product) => product.isRecommended)
+                  .toList(),
+            ),
+            const SectionTitle(title: 'MOST POPULAR'),
+            ProductCarousel(
+              products: Product.products
+                  .where((product) => product.isPopular)
+                  .toList(),
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/widgets/product_carousel.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../models/models.dart';
+import 'widgets.dart';
+
+class ProductCarousel extends StatelessWidget {
+  final List<Product> products;
+  const ProductCarousel({
+    Key? key,
+    required this.products,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.topLeft,
+      child: SizedBox(
+        height: 165,
+        child: ListView.builder(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20.0,
+            vertical: 10.0,
+          ),
+          shrinkWrap: true,
+          scrollDirection: Axis.horizontal,
+          itemCount: products.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.only(right: 5.0),
+              child: ProductCard(
+                product: products[index],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
