5b8a7cc0af89fbf6e7bf83dd96957c232a95f562
redantcode2021 <redantcyber@gmail.com>
2023-02-23 05:46:19 +0700

    8-create-wishlist-screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:contoso_ecom/blocs/blocs.dart';
 import 'package:contoso_ecom/config/app_router.dart';
 import 'package:contoso_ecom/config/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'screens/screens.dart';
 
@@ -13,13 +15,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Contoso E-comm',
-      theme: theme(),
-      onGenerateRoute: AppRouter.onGenerateRoute,
-      initialRoute: HomeScreen.routeName,
-      home: const HomeScreen(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => WishlistBloc()..add(StartWishlist()),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Contoso E-comm',
+        theme: theme(),
+        onGenerateRoute: AppRouter.onGenerateRoute,
+        initialRoute: HomeScreen.routeName,
+        home: const HomeScreen(),
+      ),
     );
   }
 }
--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -5,10 +5,14 @@ import '../models/models.dart';
 class ProductCard extends StatelessWidget {
   final Product product;
   final double widthFactor;
+  final double leftPosition;
+  final bool isWishlist;
   const ProductCard({
     Key? key,
     required this.product,
     this.widthFactor = 2.5,
+    this.leftPosition = 5,
+    this.isWishlist = false,
   }) : super(key: key);
 
   @override
@@ -31,9 +35,9 @@ class ProductCard extends StatelessWidget {
           ),
           Positioned(
             top: 60,
-            left: 5,
+            left: leftPosition,
             child: Container(
-              width: widthValue,
+              width: widthValue - 5 - leftPosition,
               height: 80,
               decoration: BoxDecoration(
                 color: Colors.black.withAlpha(50),
@@ -42,48 +46,62 @@ class ProductCard extends StatelessWidget {
           ),
           Positioned(
             top: 65,
-            left: 5,
+            left: leftPosition + 5,
             child: Container(
-              width: widthValue - 10,
+              width: widthValue - 15 - leftPosition,
               height: 70,
               decoration: const BoxDecoration(
                 color: Colors.black,
               ),
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 3,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          product.name,
-                          style:
-                              Theme.of(context).textTheme.headline4!.copyWith(
-                                    color: Colors.white,
-                                  ),
-                        ),
-                        Text(
-                          '\$${product.price}',
-                          style:
-                              Theme.of(context).textTheme.headline6!.copyWith(
-                                    color: Colors.white,
-                                  ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.add_circle,
-                        color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            product.name,
+                            style:
+                                Theme.of(context).textTheme.headline4!.copyWith(
+                                      color: Colors.white,
+                                    ),
+                          ),
+                          Text(
+                            '\$${product.price}',
+                            style:
+                                Theme.of(context).textTheme.headline6!.copyWith(
+                                      color: Colors.white,
+                                    ),
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                ],
+                    Expanded(
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.add_circle,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    isWishlist
+                        ? Expanded(
+                            child: IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.remove_circle,
+                                color: Colors.white,
+                              ),
+                            ),
+                          )
+                        : SizedBox(),
+                  ],
+                ),
               ),
             ),
           ),
