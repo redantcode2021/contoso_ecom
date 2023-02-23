4264eeef51a16c753e4f62b8569c5f070f6d4b15
redantcode2021 <redantcyber@gmail.com>
2023-02-23 09:33:07 +0700

    9-create-cart-screen

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -1,3 +1,4 @@
+import 'package:contoso_ecom/models/models.dart';
 import 'package:flutter/material.dart';
 
 import '../../widgets/widgets.dart';
@@ -7,8 +8,8 @@ class CartScreen extends StatelessWidget {
 
   static Route route() {
     return MaterialPageRoute(
-      settings: RouteSettings(name: routeName),
-      builder: (_) => CartScreen(),
+      settings: const RouteSettings(name: routeName),
+      builder: (_) => const CartScreen(),
     );
   }
 
@@ -21,7 +22,149 @@ class CartScreen extends StatelessWidget {
         title: 'Cart',
         automaticallyImplyLeading: true,
       ),
-      bottomNavigationBar: CustomNavBar(),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: Container(
+          height: 70,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
+                onPressed: () {},
+                child: Text(
+                  'GO TO CHECKOUT',
+                  style: Theme.of(context).textTheme.headline3,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20.0,
+          vertical: 10.0,
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  Cart().freeDeliveryString,
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      shape: const RoundedRectangleBorder(),
+                      elevation: 0,
+                    ),
+                    child: Text(
+                      "Add More Items",
+                      style: Theme.of(context).textTheme.headline5!.copyWith(
+                            color: Colors.white,
+                          ),
+                    ))
+              ],
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              height: 375,
+              child: ListView.builder(
+                  itemCount: Cart().products.length,
+                  itemBuilder: (context, index) {
+                    return CartProductCard(product: Cart().products[index]);
+                  }),
+            ),
+            Column(
+              children: [
+                const Divider(thickness: 2),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'SUBTOTAL',
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                      Text(
+                        '\$${Cart().subtotalString}',
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'DELIVERY FEE',
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                      Text(
+                        '\$${Cart().deliveryFeeString}',
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 10),
+                Stack(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        color: Colors.black.withAlpha(50),
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.all(5.0),
+                      height: 50,
+                      decoration: const BoxDecoration(
+                        color: Colors.black,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'TOTAL',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline5!
+                                  .copyWith(color: Colors.white),
+                            ),
+                            Text(
+                              '\$${Cart().totalString}',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline5!
+                                  .copyWith(color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
