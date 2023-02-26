feebb6e3371a2b7a0a8b0f3d2333c59e523f6614
redantcode2021 <redantcyber@gmail.com>
2023-02-26 09:08:51 +0700

    Setting Firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,15 @@ import 'package:contoso_ecom/app_bloc_observer.dart';
 import 'package:contoso_ecom/blocs/blocs.dart';
 import 'package:contoso_ecom/config/app_router.dart';
 import 'package:contoso_ecom/config/theme.dart';
-import 'package:contoso_ecom/screens/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'screens/screens.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   Bloc.observer = AppBlocObserver();
   runApp(const MyApp());
 }
